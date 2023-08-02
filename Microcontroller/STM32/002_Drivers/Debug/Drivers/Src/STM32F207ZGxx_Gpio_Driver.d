42604068701e637496a4db5211c6228f1ca3e57a
MOHZZAM <100450336+MOHZZAM@users.noreply.github.com>
2023-08-02 20:12:07 +0530

    Removed addition files from STM32

--- Microcontroller/STM32/002_Drivers/Debug/Drivers/Src/STM32F207ZGxx_Gpio_Driver.d
+++
@@ -1,6 +0,0 @@
-Drivers/Src/STM32F207ZGxx_Gpio_Driver.o: \
- ../Drivers/Src/STM32F207ZGxx_Gpio_Driver.c \
- D:/MD/Learning/Learning/STM32/002_Drivers/Drivers/Inc/STM32F207ZGxx_Gpio_Driver.h \
- D:/MD/Learning/Learning/STM32/002_Drivers/Drivers/Inc/STM32F207ZGxx.h
-D:/MD/Learning/Learning/STM32/002_Drivers/Drivers/Inc/STM32F207ZGxx_Gpio_Driver.h:
-D:/MD/Learning/Learning/STM32/002_Drivers/Drivers/Inc/STM32F207ZGxx.h:
--- Microcontroller/STM32/002_Drivers/Debug/Drivers/Src/STM32F207ZGxx_SPI_Driver.d
+++
@@ -1,6 +0,0 @@
-Drivers/Src/STM32F207ZGxx_SPI_Driver.o: \
- ../Drivers/Src/STM32F207ZGxx_SPI_Driver.c \
- D:/MD/Learning/Learning/STM32/002_Drivers/Drivers/Inc/STM32F207ZGxx_SPI_Driver.h \
- D:/MD/Learning/Learning/STM32/002_Drivers/Drivers/Inc/STM32F207ZGxx.h
-D:/MD/Learning/Learning/STM32/002_Drivers/Drivers/Inc/STM32F207ZGxx_SPI_Driver.h:
-D:/MD/Learning/Learning/STM32/002_Drivers/Drivers/Inc/STM32F207ZGxx.h:
